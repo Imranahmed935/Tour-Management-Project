0ae4a8b7c20a3c8f91ac9a04e17e3d5c69d4fb8d
Imranahmed935 <imrantahir9918@gmail.com>
2025-07-24 23:51:05 +0600

    project setup done

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,26 @@
 import { Server } from "http";
-
 import mongoose from "mongoose";
 import app from "./app";
-import { promise } from "zod";
-import { error } from "console";
+import { envVars } from "./app/config/env";
+// import { promise } from "zod";
+// import { error } from "console";
 
 let server: Server;
 
 const runServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://mongoTodo:mongodb@cluster0.haqk7.mongodb.net/tour-backend?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    console.log(envVars.NODE_ENV);
+    await mongoose.connect(envVars.DB_URL);
     console.log("Database Connected successfully!");
 
-    server = app.listen(5000, () => {
-      console.log(`The server is Running!!`);
+    server = app.listen(envVars.PORT, () => {
+      console.log(`The server is Running on port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
   }
 };
 runServer();
-
 
 // process.on("SIGINT", (err) => {
 //   console.log("SIGINT detected. server shuting down...", err);
@@ -34,7 +32,6 @@ runServer();
 //   }
 // });
 
-
 process.on("SIGTERM", (err) => {
   console.log("SIGTERM detected. server shuting down...", err);
   if (server) {
@@ -44,7 +41,6 @@ process.on("SIGTERM", (err) => {
     process.exit(1);
   }
 });
-
 
 process.on("unhandledRejection", (err) => {
   console.log("unhandled rejection detected. server shuting down...", err);
@@ -56,8 +52,7 @@ process.on("unhandledRejection", (err) => {
   }
 });
 
-
-process.on("uncaughtException",(err)=>{
+process.on("uncaughtException", (err) => {
   console.log("uncaugth Exception detected. server shuting down...", err);
   if (server) {
     server.close(() => {
@@ -65,7 +60,7 @@ process.on("uncaughtException",(err)=>{
     });
     process.exit(1);
   }
-})
+});
 
 // Promise.reject(new Error("I forgot to catch this promise!"));
 // throw new Error("i forgot to handle local error")
