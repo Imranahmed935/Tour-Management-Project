0ef25a3b9dcd7a9062fecdaf66d9f939f7be564b
Imranahmed935 <imrantahir9918@gmail.com>
2025-08-25 01:50:44 +0600

    part-6 added

---
+++ src/app/modules/sslCommerz/sslCommerz.interface.ts
@@ -0,0 +1,16 @@
+// export interface ISSLCommerz {
+//     amount: number;
+//     transactionId: string;
+//     name: string,
+//     email: string,
+//     phoneNumber: string;
+//     address: string
+// }
+export interface ISSLCommerz {
+    amount:number;
+    transactionId:string,
+    name:string,
+    email:string,
+    phoneNumber:string,
+    address:string
+}
